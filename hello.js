3df0e6b3f79450804e50613bf7191b495500b2d4
Carlos Bello <bellopauste@yahoo.com>
2017-01-26 22:22:23 +0100

    Requesting interaction

--- hello.js
+++ hello.js
@@ -3,6 +3,11 @@ const builder = require('botbuilder');
 const connector = new builder.ConsoleConnector().listen();
 const bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', session => {
-    session.send('Hello, bot!');
-});
+bot.dialog('/', [
+    session => {
+        builder.Prompts.text(session, 'Please, enter your name');
+    },
+    (session, result) => {
+        session.send(`You say: ${result.response}`);
+    },
+]);
