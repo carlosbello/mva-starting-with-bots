0b1951f72c1049f2a4854c726cf6bf4ed2bb6da3
Carlos Bello <bellopauste@yahoo.com>
2017-01-28 21:30:16 +0100

    Adds list of options to choise

--- hello.js
+++ hello.js
@@ -3,13 +3,22 @@ const restify = require('restify');
 
 const connector = new builder.ChatConnector();
 const bot = new builder.UniversalBot(connector);
+const CV_SECTIONS = [
+    'Contact information',
+    'Skills',
+    'Work experience',
+    'Education and training',
+];
 
 bot.dialog('/', [
     session => {
-        builder.Prompts.text(session, 'Please, enter your name');
+        builder.Prompts.text(session, 'What is your name?');
     },
     (session, result) => {
-        session.send(`You say: ${result.response}`);
+        builder.Prompts.choice(session, `Hello ${result.response}, what do you want to know about me?`, CV_SECTIONS);
+    },
+    (session, result) => {
+        session.send(result.response.entity);
     },
 ]);
 
